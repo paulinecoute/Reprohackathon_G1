558d4744b6a95b8254c26fe85f49bc88e93f4eca
paulinecoute <pauline.coute@universite-paris-saclay.fr>
2025-11-14 14:10:23 +0100

    ajout de la table avec les gènes traduction plus complète que sau00970 et sau03010 mais la conserver pour le moment, et ajout du script R pour le MAplot mais il sera vrm à améliorer c'est juste le début, à refaire avec ggplot2 ?

--- MAplot_Pauline.R
+++ MAplot_Pauline.R
@@ -1,55 +1,30 @@
 library(DESeq2)
 library(tidyverse)
 
-#############################################
-### 1) Charger ton fichier counts annotés
-#############################################
-
 counts <- read_delim("counts_annotated.txt", delim="\t")
 
-# Extraire la matrice de comptage (seulement les colonnes des 6 conditions)
 count_matrix <- counts %>%
-  select(ctrl4, ctrl5, ctrl6, IP1, IP2, IP3)
+  select(ctrl4, ctrl5, ctrl6, IP1, IP2, IP3) # que les colonnes des comptages 
 
-# Geneid comme rownames
 rownames(count_matrix) <- counts$Geneid
 
-#############################################
-### 2) Construire colData (design)
-#############################################
-
-colData <- data.frame(
-  condition = factor(c("ctrl","ctrl","ctrl","IP","IP","IP"))
-)
+colData <- data.frame(condition = factor(c("ctrl","ctrl","ctrl","IP","IP","IP")))
 rownames(colData) <- colnames(count_matrix)
 
-#############################################
-### 3) Construire l'objet DESeq2
-#############################################
-
+# objet deseq2
 dds <- DESeqDataSetFromMatrix(
   countData = count_matrix,
   colData = colData,
   design = ~ condition
 )
 
-#############################################
-### 4) Pré-filtrage
-#############################################
-dds <- dds[rowSums(counts(dds)) >= 10, ]
+dds <- dds[rowSums(counts(dds)) >= 10, ] #prefiltrer 
 
-#############################################
-### 5) Définir la condition de référence
-#############################################
-dds$condition <- relevel(dds$condition, ref="ctrl")
+dds$condition <- relevel(dds$condition, ref="ctrl") # controle est la ref f
 
-#############################################
-### 6) DESeq
-#############################################
+
 dds <- DESeq(dds)
 res <- results(dds)
 
-#############################################
-### 7) MA plot
-#############################################
+#ma plot 
 plotMA(res, ylim=c(-4,4))
