81e83c489d84a5cba5a93a2ce6641a04a7817cd8
paulinecoute <pauline.coute@universite-paris-saclay.fr>
2025-11-26 21:31:34 +0100

    dernier docker build et 2e snakefile

--- Figures.R
+++ Figures.R
@@ -2,11 +2,13 @@
 # MA plot sur tous les gènes
 # MA plot sur les gènes impliqués dans la traduction 
 
+# un volcano plot est également effectué 
+
 # Inputs 
 # Table de comptage data_counts/counts.txt 
-# Liste des gènes impliqués dans la traduction : data_others/genes_translation.txt
+# Liste des gènes impliqués dans la traduction : others/genes_translation.txt
 
-dir.create("plots") # dossier dans lequel seront mises les figures
+dir.create("figures") # dossier dans lequel seront mises les figures
 
 # Chargement et Préparation de notre table 
 count_data=read.table("data_counts/counts.txt", header = TRUE, row.names = 1)
@@ -43,13 +45,13 @@ plot(
 axis(1, at = c(1, 100, 10000), labels = expression(10^0, 10^2, 10^4))
 abline(h = 0, col = "black", lty = 2)
 
-dev.copy(png, filename = "plots/MAplot_all.png", width = 600, height = 500) # dimensions qui se rapporchent de celles des auteurs 
+dev.copy(png, filename = "figures/MAplot_all.png", width = 600, height = 500) # dimensions qui se rapporchent de celles des auteurs 
 dev.off()
 
 # MA plot des gènes impliqués dans la traduction 
 
 # Chargement et Préparation du fichier avec les noms des gènes de la traduction 
-translation=scan( "data_others/genes_translation.txt",what = "character", comment.char = "#", quiet = TRUE)
+translation=scan( "others/genes_translation.txt",what = "character", comment.char = "#", quiet = TRUE)
 translation_ids = translation[grepl("^SAOUHSC_", translation)] # valides, sans les com etc c'est juste une vérification supplémentaire 
 res$GeneID = sub("^gene-", "", rownames(res))  # enlever "gene-" de l'ID
 res_translation = res[res$GeneID %in% translation_ids, ] # on sélectionne les gènes présents dans notre table de comptage
@@ -80,7 +82,7 @@ legend("bottomleft",legend = c("Significant", "Non-significant"),col = c("red", 
 
 # Ajout du nom des quelques gènes présents sur le graphique des auteurs 
 
-translation_lines=readLines("genes_translation.txt")
+translation_lines=readLines("others/genes_translation.txt")
 translation_data=translation_lines[grepl("^SAOUHSC_", translation_lines)]
 
 parsed_translation <- do.call(rbind, lapply(translation_data, function(line) {
@@ -146,8 +148,108 @@ points(
 usr=par("usr") 
 legend(x = usr[2] - 10,  y = usr[3] + 1,legend = c("AA-tRNA synthetase"),pch = 21,pt.bg = "grey",col = "black",lwd = 1.5,pt.cex = 1,bty = "n",cex = 0.8 )
 
-dev.copy(png, filename = "plots/MAplot_translation.png", width = 400, height = 400)
+dev.copy(png, filename = "figures/MAplot_translation.png", width = 400, height = 400)
 dev.off()
 
+## Volcano plots 
 
+# Dataset propre
+res_df <- as.data.frame(res)
+res_df <- res_df[!is.na(res_df$padj), ]
+
+# Comptage up/down
+sig <- res_df[res_df$padj < 0.05, ]
+up  <- sum(sig$log2FoldChange > 0)
+down <- sum(sig$log2FoldChange < 0)
+
+##############
+# Volcano all
+##############
+png("figures/Volcano_all.png", width = 650, height = 500)
+
+with(res_df, plot(
+  x = log2FoldChange,
+  y = -log10(padj),
+  pch = 20,
+  cex = 0.6,
+  col = ifelse(padj < 0.05 & abs(log2FoldChange) > 1, "red", "grey"),
+  xlab = "Log2 Fold Change (Treated vs Control)",
+  ylab = "-log10(FDR)",
+  xlim = c(-6, 6),
+  main = paste0("Volcano plot – All genes\nUp: ", up, "   Down: ", down)
+))
+
+abline(v = c(-1, 1), col = "darkgrey", lty = 2)
+abline(h = -log10(0.05), col = "darkgrey", lty = 2)
+
+dev.off()
+
+##############################
+# Prépa volcano translation
+##############################
+
+res_translation_df <- as.data.frame(res_translation)
+res_translation_df <- res_translation_df[!is.na(res_translation_df$padj), ]
+
+# Top 10
+top10 <- res_translation_df[order(res_translation_df$padj), ][1:10, ]
+top10 <- merge(top10, parsed_translation, by.x = "GeneID", by.y = "locus_tag", all.x = TRUE)
+top10$label <- ifelse(is.na(top10$symbol), top10$GeneID, top10$symbol)
+
+###########################
+# Volcano translation
+###########################
+png("figures/Volcano_translation.png", width = 500, height = 500)
+
+with(res_translation_df, plot(
+  x = log2FoldChange,
+  y = -log10(padj),
+  pch = 20,
+  cex = 0.7,
+  col = ifelse(padj < 0.05 & abs(log2FoldChange) > 1, "red", "grey"),
+  xlab = "Log2 Fold Change (Treated vs Control)",
+  ylab = "-log10(FDR)",
+  main = "Volcano plot – Translation genes",
+  xlim = c(-6, 6),
+  ylim = c(0, max(-log10(padj), na.rm = TRUE) + 1)
+))
+
+abline(v = c(-1, 1), col = "darkgrey", lty = 2)
+abline(h = -log10(0.05), col = "darkgrey", lty = 2)
+
+# Labels top10
+for (i in 1:nrow(top10)) {
+  x = top10$log2FoldChange[i]
+  y = -log10(top10$padj[i])
+  text(x, y + 0.5, top10$label[i], cex = 0.8, font = 2, col="green")
+  arrows(x0 = x, y0 = y + 0.4, x1 = x, y1 = y, length = 0.08)
+}
+
+# Labels gènes d’intérêt (target_symbols)
+for (i in 1:nrow(genes_to_label)) {
+  gene_id <- genes_to_label$locus_tag[i]
+  row <- res_translation_df[res_translation_df$GeneID == gene_id, ]
+  if (nrow(row) == 1) {
+    x = row$log2FoldChange
+    y = -log10(row$padj)
+    text(x, y + 0.4, genes_to_label$symbol[i], cex = 0.85, font = 2)
+    arrows(x0 = x, y0 = y + 0.3, x1 = x, y1 = y, length = 0.08)
+  }
+}
+
+# Points aa–tRNA synthetases
+points(
+  x = res_translation_df$log2FoldChange[res_translation_df$AA_tRNA],
+  y = -log10(res_translation_df$padj[res_translation_df$AA_tRNA]),
+  pch = 21,
+  bg = ifelse(res_translation_df$padj[res_translation_df$AA_tRNA] < 0.05, "red", "grey"),
+  col = "black",
+  lwd = 1.5,
+  cex = 1
+)
+
+legend("topright", legend = "AA-tRNA synthetase", pch = 21,
+       pt.bg = "grey", col = "black", bty = "n", cex = 0.8)
+
+dev.off()
 
